3d47e520ea4e65be9486a89289e90a27c36e0c4b
Soli Lee <64163685+dlthfl87@users.noreply.github.com>
2021-12-17 20:33:28 +0000

    Explored alternate approach for pair with target sum

--- index.js
+++ index.js
@@ -1,22 +1,97 @@
-//12/16 find subsets 
-const find_subsets = (nums) => {
-  let subsets = [];
-  subsets.push([]);
+// 12/18 pair with target sum
+// Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
 
-  for (let i = 0; i < nums.length; i++) {
-    const currentNumber = nums[i];
-    const n = subsets.length;
-    for (let j = 0; j < n; j++) {
-      let copy = subsets[j].slice();
-      copy.push(currentNumber);
-      subsets.push(copy);
+// // Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+// const pair_with_targetsum = function(arr, target_sum) {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     if (arr[left] + arr[right] === target_sum) {
+//       return [left, right];
+//     } else if (arr[left] + arr[right] > target_sum) {
+//       right--;
+//     } else if (arr[left] + arr[right] < target_sum) {
+//       left++;
+//     }
+//   }
+//   return [-1, -1];
+// }
+// console.log(pair_with_targetsum([1, 2, 3, 4, 6], 6)) //output: [1, 3]
+// console.log(pair_with_targetsum([2, 5, 9, 11], 11)) //output: [0 , 2]
+
+//time complexity is O (N) where N is the total number of elemenets in the given array. space complexity in constant space O(1);
+
+const pair_with_targetsum = function(arr, target_sum) {
+  //target_sum - arr[i] = corrNum 
+  //search for corrNum in hashtable;
+  //for each value that doesnt have corrNum, store key in hashtable with index as value
+  //if corrNum found, return indexes
+  const nums = {};
+  for (var elements in arr) {
+    const corrNum = target_sum - arr[elements];
+    if (nums[corrNum]) {
+      return [nums[corrNum], elements];
     }
+    nums[arr[elements]] = elements;
   }
-  return subsets;
+  return [-1, -1];
 }
 
-console.log(find_subsets([1, 3])) //expected output: [[], [ 1 ], [ 3 ], [ 1, 3 ] ]
-console.log(find_subsets([1, 5, 3])) //expected output: [ [], [ 1 ], [ 5 ], [ 1, 5 ], [ 3 ], [ 1, 3 ], [ 5, 3 ], [ 1, 5, 3 ] ]
+console.log(pair_with_targetsum([1, 2, 3, 4, 6], 6)) //output: [1, 3]
+console.log(pair_with_targetsum([2, 5, 9, 11], 11)) //output: [0 , 2]
+
+//12/17 subsets with duplicates
+// // Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+// const find_subsets = (nums) => {
+//   let subsets = [];
+//   //create a set
+//   //to the set, push each value from subset to set
+//   //reset subset to all values in the set. 
+//   let set = new Set();
+//   subsets.push([]);
+//   set.add([]);
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentNumber = nums[i];
+//     let n = subsets.length;
+//     for (let j = 0; j < n; j++) {
+//       var copy = subsets[j].slice(0);
+//       copy.push(currentNumber);
+//       set.add(copy);
+//       subsets.push(copy);
+//     }
+//   }
+
+//   subsets = [];
+//   for (let element of set) {
+//     subsets.push(element);
+//   }
+ 
+//   return subsets;
+// };
+// //arrays are not equal to each other. cant use set to determine if there are duplicate values;
+// console.log(find_subsets([1, 3, 3])) // should output: [], [1], [3], [1,3], [3,3], [1,3,3]
+ 
+
+
+//12/16 find subsets 
+// const find_subsets = (nums) => {
+//    let subsets = [];
+//   subsets.push([]);
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const currentNumber = nums[i];
+//     const n = subsets.length;
+//     for (let j = 0; j < n; j++) {
+//       let copy = subsets[j].slice();
+//       copy.push(currentNumber);
+//       subsets.push(copy);
+//     }
+//   }
+//   return subsets;
+// }
+
+// console.log(find_subsets([1, 3])) //expected output: [[], [ 1 ], [ 3 ], [ 1, 3 ] ]
+// console.log(find_subsets([1, 5, 3])) //expected output: [ [], [ 1 ], [ 5 ], [ 1, 5 ], [ 3 ], [ 1, 3 ], [ 5, 3 ], [ 1, 5, 3 ] ]
 
 
 //String Anagrams 10/20
@@ -224,6 +299,29 @@ console.log(find_subsets([1, 5, 3])) //expected output: [ [], [ 1 ], [ 5 ], [ 1,
 // 10/31 triplet sum to 0
 // Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
+// const search_triplets = function(arr) {
+//   triplets = [];
+//   arr.sort((a,b) => { return a - b })
+//   console.log(arr)
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     if (arr[left] > right) {
+//       right--
+//     }
+//   }
+
+//   return triplets;
+// };
+
+//three values that when added equal 0;
+//want to sort the numbers from least to greatest;
+//find one or two values that when added equals the negative sign of that sum
+//-3 = -(1 + 2)
+// -z = x + y
+
+// console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2])) //[-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
+
 //arr needs to be sorted in order
 //at each end whichever number is greater, look for numbers on the opposite end that when added equal that number.
 
@@ -262,6 +360,8 @@ console.log(find_subsets([1, 5, 3])) //expected output: [ [], [ 1 ], [ 5 ], [ 1,
 
 //   return triplets;
 // };
+
+
 
 //10/2 search triplets review
 //1) need to search each number in arr for two other pairs that gives a sum of 0;
@@ -309,3 +409,173 @@ console.log(find_subsets([1, 5, 3])) //expected output: [ [], [ 1 ], [ 5 ], [ 1,
 // console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2])) //expected output [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
 // console.log(search_triplets([-5, 2, -1, -2, 3]));
 // //expected output => [[-5, 2, 3], [-2, -1, 3]]
+// Given an array of unsorted numbers and a target number, find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet. If there are more than one such triplet, return the sum of the triplet with the smallest sum.
+
+// const triplet_sum_close_to_target = function (arr, target_sum) {
+//   return -1;
+// }
+
+// function triplet_sum_close_to_target(arr, targetSum) {
+//   arr.sort((a, b) => a - b);
+//   let smallest_difference = Infinity;
+//   for (let i = 0; i < arr.length - 2; i++) {
+//     let left = i + 1,
+//       right = arr.length - 1;
+//     while (left < right) {
+//       const target_diff = targetSum - arr[i] - arr[left] - arr[right];
+//       if (target_diff === 0) { // we've found a triplet with an exact sum
+//         return targetSum - target_diff; // return sum of all the numbers
+//       }
+
+//       if (Math.abs(target_diff) < Math.abs(smallest_difference)) {
+//         smallest_difference = target_diff; // save the closest difference
+//       }
+//       // the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+//       if (Math.abs(target_diff) < Math.abs(smallest_difference) ||
+//         (Math.abs(target_diff) === Math.abs(smallest_difference) && target_diff > smallest_difference)) {
+//         smallest_difference = target_diff; // save the closest and the biggest difference
+//       }
+
+
+//       if (target_diff > 0) {
+//         left += 1; // we need a triplet with a bigger sum
+//       } else {
+//         right -= 1; // we need a triplet with a smaller sum
+//       }
+//     }
+//   }
+//   return targetSum - smallest_difference;
+// }
+
+
+// console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2)); //1
+// console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1)); //0  
+// console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100)); //3
+
+//triplets with smaller sum
+// Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.
+// Input: [-1, 0, 2, 3], target=3 
+// Output: 2
+// Explanation: There are two triplets whose sum is less than the target: [-1, 0, 3], [-1, 0, 2]
+
+// Input: [-1, 4, 2, 1, 3], target=5 
+// Output: 4
+// Explanation: There are four triplets whose sum is less than the target: 
+//    [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
+
+// const triplet_with_smaller_sum = function(arr, target) {
+//   let count = 0;
+//   // TODO: Write your code here
+//   for (var i = 0; i < arr.length - 2; i ++) {
+//     let anchor = arr[i]; 
+//     let left = i + 1; 
+//     let right = arr.length - 1; 
+//     while (left < right) {
+//     let sum = anchor + arr[left] + arr[right]; 
+//       if (sum < target) {
+//         count ++; 
+//         left ++; 
+//       }; 
+//     }
+//   }
+//   return count;
+// };
+
+
+// function triplet_with_smaller_sum(arr, target) {
+//   arr.sort((a, b) => a - b);
+//   const triplets = [];
+//   for (i = 0; i < arr.length - 2; i++) {
+//     search_pair(arr, target - arr[i], i, triplets);
+//   }
+//   return triplets;
+// }
+
+
+// function search_pair(arr, target_sum, first, triplets) {
+//   let left = first + 1,
+//     right = arr.length - 1;
+//   while ((left < right)) {
+//     if (arr[left] + arr[right] < target_sum) { // found the triplet
+//       // since arr[right] >= arr[left], therefore, we can replace arr[right] by any number between
+//       // left and right to get a sum less than the target sum
+//       for (i = right; i > left; i--) {
+//         triplets.push([arr[first], arr[left], arr[i]]);
+//       }
+//       left += 1;
+//     } else {
+//       right -= 1; // we need a pair with a smaller sum
+//     }
+//   }
+// }
+
+
+// console.log(triplet_with_smaller_sum([-1, 0, 2, 3], 3)); // [ [ -1, 0, 3 ], [ -1, 0, 2 ] ]
+// console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5)); // [ [ -1, 1, 4 ], [ -1, 1, 3 ], [ -1, 1, 2 ], [ -1, 2, 3 ] ]
+
+
+//linked list cycle
+// Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+
+
+// class Node {
+//   constructor(value, next=null){
+//     this.value = value;
+//     this.next = next;
+//   }
+// }
+
+// const has_cycle = function(head) {
+//   // TODO: Write your code here
+//   return false
+// }
+
+
+// head = new Node(1)
+// head.next = new Node(2)
+// head.next.next = new Node(3)
+// head.next.next.next = new Node(4)
+// head.next.next.next.next = new Node(5)
+// head.next.next.next.next.next = new Node(6)
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+// head.next.next.next.next.next.next = head.next.next
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+// head.next.next.next.next.next.next = head.next.next.next
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+// class Node {
+//   constructor(value, next = null) {
+//     this.value = value;
+//     this.next = next;
+//   }
+// }
+
+// function has_cycle(head) {
+//   let slow = head,
+//     fast = head;
+//   while (fast !== null && fast.next !== null) {
+//     fast = fast.next.next;
+//     slow = slow.next;
+//     if (slow === fast) {
+//       return true; // found the cycle
+//     }
+//   }
+//   return false;
+// }
+
+
+// const head = new Node(1);
+// head.next = new Node(2);
+// head.next.next = new Node(3);
+// head.next.next.next = new Node(4);
+// head.next.next.next.next = new Node(5);
+// head.next.next.next.next.next = new Node(6);
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+// head.next.next.next.next.next.next = head.next.next;
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+// head.next.next.next.next.next.next = head.next.next.next;
+// console.log(`LinkedList has cycle: ${has_cycle(head)}`);
