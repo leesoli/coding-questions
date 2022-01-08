8501c24e2e393d917b2a5ebd9590916650a7814b
Soli Lee <64163685+dlthfl87@users.noreply.github.com>
2022-01-08 19:08:33 +0000

    Cycle in circular array redo.

--- fands_pointers.js
+++ fands_pointers.js
@@ -1,35 +1,55 @@
-//12/31. Challenge Prob 3 Cycle in a circular array
+//12/31 && 1/8 Challenge Prob 3 Cycle in a circular array
 // We are given an array containing positive and negative numbers. Suppose the array contains a number ‘M’ at a particular index. Now, if ‘M’ is positive we will move forward ‘M’ indices and if ‘M’ is negative move backwards ‘M’ indices. You should assume that the array is circular which means two things:
 
 // If, while moving forward, we reach the end of the array, we will jump to the first element to continue the movement.
 // If, while moving backward, we reach the beginning of the array, we will jump to the last element to continue the movement.
 // Write a method to determine if the array has a cycle. The cycle should have more than one element and should follow one direction which means the cycle should not contain both forward and backward movements.
 
+//check for the same direction
+
 const circular_array_loop_exists = function(arr) {
-  //slow pointer for first value
-  let slow = 0;
-  let fast = 0;
-  let loop = 0;
-  //fast pointer moves forward or back however many indexes;
-  while (slow < arr.length) {
-    fast += arr[fast];
-    console.log(fast, 'fast')
-    if (fast === slow) { return true; }
-    
-    if (fast > arr.length - 1) {
-      fast = (fast - (arr.length - 1)) - 1;
-      loop++;
-      if (loop > 2) { slow++; }
+  for (let i = 0; i < arr.length; i++) {
+    let isForward = arr[i] >= 0; //true or false
+    let slow = i;
+    let fast = i;
+
+  //if not in the same direction, there is no cycle
+  while (true) {
+    slow = find_next_index(arr, isForward, slow);
+    fast = find_next_index(arr, isForward, fast); 
+    if (fast !== -1) {
+      fast = find_next_index(arr, isForward, fast);
     }
+
+    if (slow === -1 || fast === -1 || slow === fast) { break; }
+  }
+
+  if (slow !== -1 && slow === fast) { return true; }
   }
   return false;
-  
-};
+}
+
+const find_next_index = function (arr, isForward, currentIndex) {
+  let direction = arr[currentIndex] >= 0;
+
+  if (direction !== isForward) { return -1; } //there is a change in direction
+
+  nextIndex = (currentIndex + arr[currentIndex]) % arr.length;
+  if (nextIndex < 0) {
+    nextIndex += arr.length //wrap around for neg numbers
+  }
+
+  //checks to make sure cycle that pointer to same element after move is finished
+  if (nextIndex === currentIndex) {
+    nextIndex = -1;
+  }
+
+  return nextIndex;
+}
 
 
-
-// console.log(`${circular_array_loop_exists([1, 2, -1, 2, 2])}`) //true
-// console.log(`${circular_array_loop_exists([2, 2, -1, 2])}`) // true
+console.log(`${circular_array_loop_exists([1, 2, -1, 2, 2])}`) //true
+console.log(`${circular_array_loop_exists([2, 2, -1, 2])}`) // true
 console.log(`${circular_array_loop_exists([2, 1, -1, -2])}`) //false
 
 //12/30 & 12/31. Challenge Prob 2 Rearrange a linked list
