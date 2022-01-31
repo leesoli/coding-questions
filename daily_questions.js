72fc5eb902ff5490889cd8816eb6ad97f4f5a2d5
Soli Lee <64163685+dlthfl87@users.noreply.github.com>
2022-01-31 18:22:11 +0000

    equivalent folded sums in progress

--- daily_questions.js
+++ daily_questions.js
@@ -5,93 +5,42 @@
 // Output: true
 // Explanation: The array can be partitioned as [1, 5, 5] and [11].
 
-// 1        0
-// 6. 1   5.  0
-// 2^n
-// dFS
-
-// target = 11
-
-// 1.                         0
-// target = 10                target = 11
-// i = 1
-
-// 6            1             5.             0
-// target = 4.  target =10    target = 6.   target= 11
-// i = 2
-
-// i > target then 
-
-// index value will either equal target or sum + index value
-// return true
-
-
-
-
-
-// var canPartition = function(nums) {
-//   let sum = 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     sum += nums[i];
-//   }
-  
-//   if (sum % 2 === 1) { return false; }
-//   let subsetSum = sum / 2;
-
-//   let set = {};
-//   set[0] = 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     set[nums[i]] = nums[i];
+// var canPartition = function (nums) {
+//     //add up all the values in nums and find the target sum
+//     var total = 0;
+//     for (var i = 0; i< nums.length; i++) {
+//         total += nums[i];
+//     }
+//     //in the case where the sum is odd, return false
+//     if (total % 2 === 1) {
+//         return false
+//     }
+//     let target = total/2;
     
-//     for (let numbers in set) {
-//       let currentSum = set[numbers] + nums[i];
-//        console.log(currentSum, 'current sum')
-//       //if we find subsetsum return true
-//       if (currentSum === subsetSum) {
-//         return true
-//       } else if (currentSum < subsetSum) {
-//          //store only values less than subset sum in set
-//         set[currentSum] = currentSum;
+//     //add value to set, if value equals target return true
+//     let set = new Set();
+
+//     set.add(0);
+//     for (let i = 0; i < nums.length; i++) {
+//       let newSet = new Set();
+//       for (var items of set) {
+//         let currentSum = items + nums[i];
+//         if (currentSum === target) {
+//           return true;
+//         }
+//         newSet.add(items);
+//         newSet.add(currentSum);
+//         set = newSet;
 //       }
 //     }
-//   }
-//   return false;
-// };
-var canPartition = function (nums) {
-    //add up all the values in nums and find the target sum
-    var total = 0;
-    for (var i = 0; i< nums.length; i++) {
-        total += nums[i];
-    }
-    //in the case where the sum is odd, return false
-    if (total % 2 === 1) {
-        return false
-    }
-    let target = total/2;
-    
-    //add value to set, if value equals target return true
-    let set = new Set();
+//     console.log(set)
+//     return false;
+// }
 
-    set.add(0);
-    for (let i = 0; i < nums.length; i++) {
-      let newSet = new Set();
-      for (var items of set) {
-        let currentSum = items + nums[i];
-        if (currentSum === target) {
-          return true;
-        }
-        newSet.add(items);
-        newSet.add(currentSum);
-        set = newSet;
-      }
-    }
-    console.log(set)
-    return false;
-}
+// console.log(canPartition([1, 2, 5]))
 
-console.log(canPartition([1, 2, 5]))
+//1/28/2022 & 1/30/2022
 
-//1/27/2022
 //Equivalent Folded Sums
 // You are given a list of integers nums of even length. Consider an operation where you pick any number in nums and update it with a value between [1, max(nums)].
 
@@ -109,22 +58,24 @@ console.log(canPartition([1, 2, 5]))
 
 //divide the number by two.
 //find the number that equal that sum
-// const equal_sum = function (nums) {
-//   let sum = 0;
-//   for (var i = 0; i < nums.length; i++) {
-//     sum += nums[i];
-//   }
-//   let target = sum/2;
-//   let left = 0;
-//   let right = nums.length/2 - 1;
-//   let current = 0;
-//   for (var i = 0; i < right; i++) {
-//     current += nums[i];
-//   }
-//   while (right < nums.length) {
-//     if (current > )
-//     right++;
-//   }
-// }
 
-// console.log(equal_sum([1, 4, 3, 5]))
+// i = 0, 1 + 5 = 6
+// i = 1, 4 + 3 = 7
+// i = 2, 4 + 3 = 7
+// i = 3, 1 + 5 = 6
+const equal_sum = function (nums) {
+//evaluate possible sums up to half the array
+let n = nums.length / 2 - 1;
+
+//save in set
+let set = new Set();
+
+for (let i = 0; i <= n; i++) {
+  set.add(nums[i] + nums[nums.length - 1 - i]);
+}
+
+//return the size of the set - 1 because for how many different sums there are, we do 1 less operation
+return set.size - 1;
+}
+
+console.log(equal_sum([1, 4, 3, 5]))
